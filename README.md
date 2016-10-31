9fe04c8b278830c09d4735381fc04e5fb95a7390
Eric Eastwood <contact@ericeastwood.com>
2016-10-30 22:48:50 -0500

    Formula in terms of top and bottom radius

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Calculate the volume of a liquid in a conical frustum that is sitting on the smaller end.
 
-![Conical frustum](http://i.imgur.com/yigjbcB.png)
+![Conical frustum](http://i.imgur.com/oE7FRtB.png)
 
 ###[Run online](https://costava.github.io/conical-frustum-calculator/src/)
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,31 @@
 /*
  * Returns volume of liquid in a conical frustum that is resting on its smaller circular end
  * {object} o
- * - .radius - radius of smaller end
+ * - .bottomRadius - radius of smaller end on the bottom
+ * - .topRadius - radius of bigger end on the top
  * - .fillHeight - height of liquid
- * - .difference - positive difference between radiuses of circular ends
  * - .height - height of conical frustum
  */
 function conicalFrustumVolume(o) {
-	var term1 = o.radius * o.radius * o.fillHeight;
-	var term2 = (o.radius * o.difference * o.fillHeight * o.fillHeight) / o.height;
-	var term3 = (o.difference * o.difference * o.fillHeight * o.fillHeight * o.fillHeight) / (3 * o.height * o.height);
+	var difference = o.topRadius - o.bottomRadius;
+
+	var term1 = o.bottomRadius * o.bottomRadius * o.fillHeight;
+	var term2 = (o.bottomRadius * difference * o.fillHeight * o.fillHeight) / o.height;
+	var term3 = (difference * difference * o.fillHeight * o.fillHeight * o.fillHeight) / (3 * o.height * o.height);
 
 	return Math.PI * (term1 + term2 + term3);
 }
 
 var options = {};
 
-function radiusChange() {
-	options.radius = document.querySelector('.js-radius').value;
+function bottomRadiusChange() {
+	options.bottomRadius = document.querySelector('.js-bottom-radius').value;
+
+	document.querySelector('.js-volume').innerHTML = conicalFrustumVolume(options);
+}
+
+function topRadiusChange() {
+	options.topRadius = document.querySelector('.js-top-radius').value;
 
 	document.querySelector('.js-volume').innerHTML = conicalFrustumVolume(options);
 }
@@ -40,13 +48,13 @@ function fillHeightChange() {
 	document.querySelector('.js-volume').innerHTML = conicalFrustumVolume(options);
 }
 
-document.querySelector('.js-radius').addEventListener('change', radiusChange);
+document.querySelector('.js-bottom-radius').addEventListener('change', bottomRadiusChange);
+document.querySelector('.js-top-radius').addEventListener('change', topRadiusChange);
 document.querySelector('.js-height').addEventListener('change', heightChange);
-document.querySelector('.js-difference').addEventListener('change', differenceChange);
 document.querySelector('.js-fill-height').addEventListener('change', fillHeightChange);
 
-options.radius = document.querySelector('.js-radius').value;
+options.bottomRadius = document.querySelector('.js-bottom-radius').value;
+options.topRadius = document.querySelector('.js-top-radius').value;
 options.height = document.querySelector('.js-height').value;
-options.difference = document.querySelector('.js-difference').value;
 options.fillHeight = document.querySelector('.js-fill-height').value;
 document.querySelector('.js-volume').innerHTML = conicalFrustumVolume(options);
